06490b08f44997dfd9000055038b4d8f73a05415
Ruslan <kupzhasarov14@gmail.com>
2021-10-05 18:00:23 +0600

    feat: block scss file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    extends: [
-        'stylelint-config-standard',
-    ],
+    extends: ['stylelint-config-standard'],
     // add your custom config here
     // https://stylelint.io/user-guide/configuration
-    rules: {
-        indentation:          4,
-        'string-quotes':      'single',
-        'at-rule-no-unknown': [
+    rules:   {
+        indentation:                                      4,
+        'string-quotes':                                  'single',
+        'declaration-block-single-line-max-declarations': 15,
+        'at-rule-no-unknown':                             [
             true,
             {
                 ignoreAtRules: [
@@ -27,8 +26,11 @@ module.exports = {
         'declaration-block-trailing-semicolon': null,
         'no-descending-specificity':            null,
         'no-extra-semicolons':                  true,
-        'selector-pseudo-element-no-unknown':   [true, {
-            ignorePseudoElements: ['v-deep'],
-        }],
+        'selector-pseudo-element-no-unknown':   [
+            true,
+            {
+                ignorePseudoElements: ['v-deep'],
+            },
+        ],
     },
 };
